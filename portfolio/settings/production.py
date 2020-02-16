95f1e5c640df3c63fbcb7af0c7fa0aeba1bbc207
alexmwaura <alexmwaura43@gmail.com>
2020-02-16 05:50:47 +0300

    deploy to heroku

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -10,7 +10,8 @@ MODE=config("MODE", default="dev")
 
 
 DEBUG = config('DEBUG', cast=bool)
-ALLOWED_HOSTS = ['ip-address', 'www.your-website.com']
+ALLOWED_HOSTS = 'djestate.herokuapp.com'
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
